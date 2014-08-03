adb5170829e19b83a78141a3eec70ba0725fc9d8
Glen Maddern <glenmaddern@gmail.com>
2014-08-03 16:59:42 -0700

    finally figured out why my gulp was being so lol. Fixed!

--- gulpfile.js
+++ gulpfile.js
@@ -3,64 +3,55 @@ var gulp = require('gulp'),
   $ = require('gulp-load-plugins')();
 
 gulp.task('js', function () {
-  gulp.src('src/x-gif.js')
+  return gulp.src('src/x-gif.js')
     .pipe($.plumber())
     .pipe($.browserify({
       add: [ es6ify.runtime ],
       transform: ['es6ify']
     }))
-    .pipe(gulp.dest('build/x-gif'))
     .pipe($.uglify())
-    .pipe(gulp.dest('dist/x-gif'));
+    .pipe(gulp.dest('build'));
 });
 
-gulp.task('html', function () {
-  gulp.src('src/x-gif.html')
-    .pipe(gulp.dest('build/x-gif'))
-    .pipe($.rename('x-gif.local.html'))
-    .pipe(gulp.dest('dist/x-gif'));
-});
-
-gulp.task('vulcan', function () {
-  gulp.src('dist/x-gif/x-gif.local.html')
-    .pipe($.rename('x-gif.html'))
-    .pipe(gulp.dest('dist/x-gif'))
+gulp.task('vulcan', ['js', 'css'], function () {
+  return gulp.src('src/x-gif.html')
+    .pipe(gulp.dest('build'))
     .pipe($.vulcanize({dest: 'dist/x-gif', inline: true}))
     .pipe(gulp.dest('dist/x-gif'));
 })
 
 gulp.task('css', function () {
-  gulp.src('src/x-gif.scss')
+  return gulp.src('src/x-gif.scss')
     .pipe($.rubySass())
     .pipe($.autoprefixer("last 2 versions", "> 1%"))
-    .pipe(gulp.dest('build/x-gif'))
-    .pipe(gulp.dest('dist/x-gif'));
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('jade', function () {
-  gulp.src(['src/examples/**/*.jade', '!src/examples/**/_*.jade'])
+  return gulp.src(['src/examples/**/*.jade', '!src/examples/**/_*.jade'])
     .pipe($.jade({pretty: true}))
-    .pipe(gulp.dest('build'))
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('build', ['js', 'html', 'css', 'jade'], function () {
+gulp.task('assets', function () {
+  return gulp.src('src/examples/assets/**')
+    .pipe(gulp.dest('dist/assets'))
 });
 
-gulp.task('release', ['vulcan']);
+gulp.task('build', ['vulcan', 'jade', 'assets'], function () {
+});
 
 gulp.task('clean', function () {
-  gulp.src(['x-gif.html', 'build', 'dist'], {read: false})
+  return gulp.src(['x-gif.html', 'build', 'dist'], {read: false})
     .pipe($.clean())
 })
 
 gulp.task('default', ['build', 'connect'], function () {
-  gulp.watch(['src/**/*.js'], ['js']);
-  gulp.watch(['src/*.html'], ['html']);
-  gulp.watch(['src/*.scss'], ['css']);
-  gulp.watch(['dist/x-gif/x-gif.local.html', 'dist/x-gif/x-gif.js', 'dist/x-gif/x-gif.css'], ['vulcan']);
+  gulp.watch(['src/x-gif.html', 'src/**/*.js', 'src/*.scss'], ['vulcan']);
+  gulp.watch(['src/examples/**/*.jade'], ['jade']);
+  gulp.watch(['src/examples/assets/**'], ['assets']);
 
-  gulp.watch(['build/**'], function (event) {
+  gulp.watch(['dist/**'], function (event) {
     return gulp.src(event.path)
       .pipe($.connect.reload());
   });
@@ -68,7 +59,7 @@ gulp.task('default', ['build', 'connect'], function () {
 
 gulp.task('connect', function () {
   $.connect.server({
-    root: ['build'],
+    root: ['dist'],
     port: 1983,
     livereload: {port: 2983}
   })
---
+++ src/examples/assets/synched_playback.js
@@ -0,0 +1,34 @@
+var setupAudioSynching = function (audio, xGifs, metadata) {
+  var synchOffset = -0.1;
+
+  audio.addEventListener('playing', function () {
+    var beats = metadata.response.track.analysis.beats,
+      beatIndex = 0;
+    while (beats[0].start > 0) {
+      beats.unshift({
+        start: beats[0].start - beats[0].duration,
+        duration: beats[0].duration,
+        confidence: 0
+      })
+    }
+
+    var animationLoop = function () {
+      requestAnimationFrame(animationLoop);
+
+      if (beats.length > beatIndex) {
+        var currentTime = audio.currentTime + synchOffset;
+        while (beatIndex < beats.length && currentTime > beats[beatIndex].start) {
+          beatIndex++;
+        }
+        var beat = beats[beatIndex - 1];
+
+        var sinceLastBeat = currentTime - beat.start,
+          beatFraction = sinceLastBeat / beat.duration;
+        [].forEach.call(xGifs, function (xGif) {
+          xGif.clock(beatIndex, beat.duration * 1000 / audio.playbackRate, beatFraction);
+        })
+      }
+    }
+    animationLoop();
+  });
+}
