7cf1ac664b5ae8fa2055dca101c3371f040e66c2
Glen Maddern <glenmaddern@gmail.com>
2015-01-13 02:27:29 +1100

    simplest possible playback is pretty straightforward

---
+++ config.js
@@ -0,0 +1,14 @@
+System.config({
+  "paths": {
+    "*": "*.js",
+    "x-gif/*": "lib/*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "pixi.js": "github:GoodBoyDigital/pixi.js@2.2.3"
+  }
+});
+
---
+++ src/3d.js
@@ -0,0 +1,42 @@
+import PIXI from 'pixi.js'
+import Exploder from './x-gif/exploder'
+
+// create an new instance of a pixi stage
+var stage = new PIXI.Stage(0x66FF99),
+  exploder = new Exploder("explosion.gif"),
+  renderer = new PIXI.WebGLRenderer(1280, 720);
+
+// add the renderer view element to the DOM
+document.body.appendChild(renderer.view);
+
+exploder.load().then((gif) => {
+
+// create a texture from an image path
+  var textures = gif.frames.map(frame => PIXI.Texture.fromImage(frame.url)),
+    bunnies = textures.map(texture => new PIXI.Sprite(texture)),
+    currentBunny = 0
+
+  bunnies.forEach(bunny => {
+    bunny.anchor.x = 0.5
+    bunny.anchor.y = 0.5
+    bunny.position.x = 1280 / 2
+    bunny.position.y = 720 / 2
+    bunny.width = 1280
+    bunny.height = 720
+    bunny.alpha = 0
+
+    stage.addChild(bunny)
+  })
+
+  requestAnimFrame(animate)
+
+  function animate() {
+    requestAnimFrame(animate)
+    bunnies.forEach(bunny => bunny.alpha = 0)
+    let bunny = bunnies[currentBunny++ % bunnies.length]
+    bunny.alpha = 1
+
+    renderer.render(stage)
+  }
+
+})
